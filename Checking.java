25a4977634b0e7fa7765cb91f036d6d91a830e5a
BakangMonei <78691569+BakangMonei@users.noreply.github.com>
2021-07-15 15:12:39 +0200

    Add files via upload

---
+++ Checking.java
@@ -0,0 +1,16 @@
+//To change this license header, choose License Headers in Project Properties.
+//To change this template file, choose Tools | Templates
+//and open the template in the editor.
+
+public class Checking extends Account implements Withdraw{
+
+    public Checking(int accNumber, String accHolder, double balance, String branch) {
+        super(accNumber, accHolder, balance, branch);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        this.balance =balance - amount;
+    }
+
+}
---
+++ Savings.java
@@ -0,0 +1,16 @@
+//To change this license header, choose License Headers in Project Properties.
+//To change this template file, choose Tools | Templates
+//and open the template in the editor.
+
+public class Savings extends Account implements PayInterest{
+
+    public Savings(int accNumber, String accHolder, double balance, String branch) {
+        super(accNumber, accHolder, balance, branch);
+    }
+
+    @Override
+    public void payInterest(double rate){
+        this.balance+=balance*rate;
+    }
+
+}
